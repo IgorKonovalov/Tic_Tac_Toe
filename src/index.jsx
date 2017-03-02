1a83476160096417b5bd0e1bdc2de9ced55ae0d4
Igor Konovalov <konovalov.avp@gmail.com>
2017-03-02 15:09:40 +0300

    reack+socketio basic chat working

---
+++ src/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = { messages: [] };
+  }
+
+  componentDidMount () {
+    this.socket = io('/');
+    this.socket.on('message', message => {
+      this.setState({ messages: [message, ...this.state.messages]});
+    })
+  }
+
+  handleSubmit = event => {
+    const body = event.target.value;
+    if (event.keyCode === 13 && body) {
+      const message = {
+        body,
+        from: 'Me'
+      };
+      this.setState({ messages: [message, ...this.state.messages]});
+      this.socket.emit('message', body);
+      event.target.value = '';
+    }
+  }
+
+  render () {
+    const messages = this.state.messages.map((message, index) => {
+      return <li key={index}><b>{message.from}:</b>{message.body}</li>
+    })
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <input type='text' placeholder='type a message...' onKeyUp={this.handleSubmit} />
+        {messages}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: __dirname + '/src/main.js',
+  output: {
+    path: __dirname + '/',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-maps',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: ['react-html-attrs', 'transform-class-properties']
+        }
+      }
+    ]
+  }
+}
